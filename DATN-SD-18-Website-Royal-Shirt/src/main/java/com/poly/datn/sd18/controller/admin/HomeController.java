32c29d87601fcf4b9dad50f9cad51791cb05a389
truongnv02 <nguyenvantruong200602@gmail.com>
2024-02-21 18:58:29 +0700

    giao diện admin

--- DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/controller/admin/HomeController.java
+++ DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/controller/admin/HomeController.java
@@ -1,4 +1,15 @@
 package com.poly.datn.sd18.controller.admin;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
 public class HomeController {
+    @GetMapping("")
+    public String admin(){
+        return "admin/product/index";
+    }
 }
