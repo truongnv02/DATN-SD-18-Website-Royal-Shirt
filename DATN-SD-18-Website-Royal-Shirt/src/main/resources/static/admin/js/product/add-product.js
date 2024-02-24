4aad9673e92d0007204193afaac6f78f93a6f014
NCTammm <nguyencongtam2k3@gmail.com>
2024-02-24 19:59:35 +0700

    update

--- DATN-SD-18-Website-Royal-Shirt/src/main/resources/static/admin/js/product/add-product.js
+++ DATN-SD-18-Website-Royal-Shirt/src/main/resources/static/admin/js/product/add-product.js
@@ -1,43 +1,8 @@
-function readURL(input, thumbimage) {
-    if (input.files && input.files[0]) { //Sử dụng  cho Firefox - chrome
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $("#thumbimage").attr('src', e.target.result);
-        }
-        reader.readAsDataURL(input.files[0]);
-    }
-    else { // Sử dụng cho IE
-        $("#thumbimage").attr('src', input.value);
-    }
-    $("#thumbimage").show();
-    $('.filename').text($("#uploadfile").val());
-    $('.Choicefile').css('background', '#14142B');
-    $('.Choicefile').css('cursor', 'default');
-    $(".removeimg").show();
-    $(".Choicefile").unbind('click');
+var selectColor, selectSize;
 
-}
 $(document).ready(function () {
-    $(".Choicefile").bind('click', function () {
-        $("#uploadfile").click();
-
-    });
-    $(".removeimg").click(function () {
-        $("#thumbimage").attr('src', '').hide();
-        $("#myfileupload").html('<input type="file" id="uploadfile"  onchange="readURL(this);" />');
-        $(".removeimg").hide();
-        $(".Choicefile").bind('click', function () {
-            $("#uploadfile").click();
-        });
-        $('.Choicefile').css('background', '#14142B');
-        $('.Choicefile').css('cursor', 'pointer');
-        $(".filename").text("");
-    });
-})
-
-$(document).ready(function() {
     // Khởi tạo Select màu sắc và kích thước
-    var selectColor = $('#select-colors').selectize({
+    selectColor = $('#select-colors').selectize({
         maxItems: null,
         valueField: 'id',
         labelField: 'title',
@@ -45,7 +10,7 @@ $(document).ready(function() {
         create: false
     });
 
-    var selectSize = $('#select-sizes').selectize({
+    selectSize = $('#select-sizes').selectize({
         maxItems: null,
         valueField: 'id',
         labelField: 'title',
@@ -57,15 +22,15 @@ $(document).ready(function() {
     $.ajax({
         url: '/admin/rest/add-product',
         type: 'GET',
-        success: function(data) {
+        success: function (data) {
             // Tạo options màu sắc từ danh sách màu trong productListResponse
-            var colorOptions = data.colors.map(function(color) {
-                return { id: color.id, title: color.name };
+            var colorOptions = data.colors.map(function (color) {
+                return {id: color.id, title: color.name};
             });
 
             // Tạo options kích thước từ kích thước trong productListResponse
-            var sizeOptions = data.sizes.map(function(size) {
-                return { id: size.id, title: size.name };
+            var sizeOptions = data.sizes.map(function (size) {
+                return {id: size.id, title: size.name};
             });
 
             // Cập nhật options của màu sắc
@@ -78,9 +43,124 @@ $(document).ready(function() {
             selectSize[0].selectize.addOption(sizeOptions);
             selectSize[0].selectize.refreshItems();
         },
-        error: function(err) {
+        error: function (err) {
             console.error('Error call API:', err);
         }
     });
+
 });
+
+// Hàm để lấy danh sách sản phẩm chi tiết từ các mục đã chọn từ Selectize dropdowns
+function showListProductDetail() {
+    // Lấy thông tin chi tiết về các mục đã chọn từ dropdown màu sắc
+    var selectedColorItems = selectColor[0].selectize.items.map(function (item) {
+        var option = selectColor[0].selectize.getItem(item);
+        return {id: item, name: option.text()};
+    });
+
+    // Lấy thông tin chi tiết về các mục đã chọn từ dropdown kích thước
+    var selectedSizeItems = selectSize[0].selectize.items.map(function (item) {
+        var option = selectSize[0].selectize.getItem(item);
+        return {id: item, name: option.text()};
+    });
+
+    var listProductDetail = $("#listProductDetail");
+
+    // Xóa tất cả các phần tử trong tbody
+    listProductDetail.empty();
+
+    // Biến đếm STT
+    var sttCounter = 1;
+
+    // Lấy giá trị của ô nhập liệu Tên sản phẩm
+    var productName = $("#input-product-name").val();
+
+    // Gọi hàm khác hoặc thực hiện các xử lý khác với listColor và listSize ở đây
+    for (var i = 0; i < selectedColorItems.length; i++) {
+        for (var j = 0; j < selectedSizeItems.length; j++) {
+
+            var productDetailName = productName + ' [' +
+                '<strong>' + selectedColorItems[i].name + '</strong>' + ' - ' +
+                '<strong>' + selectedSizeItems[j].name + '</strong>' + ']';
+
+            var productDetail = $(
+                '<tr>\n' +
+                '    <td class="stt">' + sttCounter + '</td>\n' +
+                '    <td class="ten_danh_muc">' + productDetailName + '</td>\n' +
+                '    <td class="so_luong"><input type="number" value="10" min="0"></td>\n' +
+                '    <td class="gia_ban" id="gia_ban"><input type="number" data-raw-value="1000000" min="0"></td>\n' +
+                '    <td class="table-td-center tinh_nang">\n' +
+                '        <button class="btn btn-primary btn-sm trash" id="removeProductDetail" type="button" title="Xóa">' +
+                '            <i\n class="fas fa-trash-alt"></i>\n' +
+                '        </button>\n' +
+                '    </td>' +
+                '</tr>'
+            );
+            $(productDetail).appendTo("#listProductDetail");
+
+            // Tăng biến đếm STT
+            sttCounter++;
+
+            // Thêm sự kiện click cho nút xóa
+            $(productDetail).find('.trash').click(function () {
+                $(this).closest('tr').remove(); // Xóa hàng chứa nút xóa được click
+                updateSTT(); // Cập nhật lại giá trị STT
+            });
+        }
+    }
+}
+
+
+
+// Hàm để cập nhật lại giá trị STT cho tất cả các hàng trong bảng
+function updateSTT() {
+    $('#listProductDetail tr').each(function (index, row) {
+        $(row).find('.stt').text(index + 1);
+    });
+}
+
+function readURL(input) {
+    if (input.files && input.files.length > 0) {
+        for (var i = 0; i < input.files.length; i++) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                // Tạo một container cho ảnh và nút xóa
+                var imageContainer = $(
+                    '<div class="image-product-container">' +
+                    '   <img src="' + e.target.result + '" alt="Thumb image" class="thumbimage"/>' +
+                    '   <a class="removeimg" href="javascript:" style="display: inline"></a>' +
+                    '</div>'
+                );
+
+                // Thêm container vào thumbbox
+                $("#thumbbox").append(imageContainer);
+
+                // Sự kiện click cho nút xóa
+                imageContainer.find(".removeimg").on("click", function () {
+                    $(this).closest(".image-product-container").remove();
+                    $("#myfileupload").html('<input type="file" id="uploadfile" name="ImageUpload" multiple onchange="readURL(this)"/>');
+                    $('.Choicefile').css('background', '#14142B');
+                    $('.Choicefile').css('cursor', 'pointer');
+                    $(".filename").text("");
+                });
+            };
+
+            reader.readAsDataURL(input.files[i]);
+        }
+    }
+
+    // Hiển thị các phần tử khi có ảnh
+    $(".Choicefile").css('background', '#14142B');
+    $(".Choicefile").css('cursor', 'default');
+}
+
+$(document).ready(function () {
+    $(".Choicefile").bind('click', function () {
+        $("#uploadfile").click();
+
+    });
+})
+
+
 
