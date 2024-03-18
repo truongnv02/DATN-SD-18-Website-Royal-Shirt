8865ee49e1a905937bc0355a5e80b87998c70846
NCTammm <nguyencongtam2k3@gmail.com>
2024-03-18 17:40:07 +0700

    update

--- DATN-SD-18-Website-Royal-Shirt/src/main/resources/static/admin/js/counter/counter.js
+++ DATN-SD-18-Website-Royal-Shirt/src/main/resources/static/admin/js/counter/counter.js
@@ -422,7 +422,7 @@ async function saveOrder() {
     var customerId = $("#select-customer").val();
     var note = $("#input-note").val();
     var shopping = $("#select-shopping").val()
-    var status = 0;
+    var status = 5;
     var successDate = moment().format("YYYY-MM-DDTHH:mm:ss");
 
     if (!checkInputSaveOrder(staffId, customerId, totalPrice, shopping)) {
