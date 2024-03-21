6e623c5783376d944fde02f64febff20fa52afc7
tamdvph29277 <tamtihon27@gmail.com>
2024-03-21 15:40:29 +0700

    update

--- DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/service/ProductDetailService.java
+++ DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/service/ProductDetailService.java
@@ -1,4 +1,10 @@
 package com.poly.datn.sd18.service;
 
+import java.util.List;
+
+import com.poly.datn.sd18.entity.ProductDetail;
+
 public interface ProductDetailService {
+    List<ProductDetail> getListProduct();
+
 }
