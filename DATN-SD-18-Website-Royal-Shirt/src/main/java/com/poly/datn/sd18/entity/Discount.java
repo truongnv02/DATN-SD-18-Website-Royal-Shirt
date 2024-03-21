28b3874f635a7f5772a36c92d2ed30f4ca70e0b7
tamdvph29277 <tamtihon27@gmail.com>
2024-03-21 15:43:26 +0700

    update

--- DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/entity/Discount.java
+++ DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/entity/Discount.java
@@ -5,6 +5,9 @@ import lombok.*;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Getter
 @Setter
@@ -25,15 +28,17 @@ public class Discount extends BaseEntity implements Serializable {
     @Column(name = "status")
     private Integer status;
 
-    @Column(name = "description")
-    private String description;
-
     @Column(name = "discount")
-    private Float discount;
+    private Integer discount;
 
     @Column(name = "start_date")
     private Date startDate;
 
     @Column(name = "end_date")
     private Date endDate;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "discount")
+    private List<Product> product;
+
 }
