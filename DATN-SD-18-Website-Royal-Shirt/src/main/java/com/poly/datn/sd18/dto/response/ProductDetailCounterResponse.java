b44e15c45326c5008f3fd7c7f863e927bb5300c8
tamdvph29277 <tamtihon27@gmail.com>
2024-03-21 15:40:41 +0700

    update

---
+++ DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/dto/response/ProductDetailCounterResponse.java
@@ -0,0 +1,12 @@
+package com.poly.datn.sd18.dto.response;
+
+public interface ProductDetailCounterResponse {
+    Integer getId();
+    String getName();
+    String getImage();
+    String getColorName();
+    String getSizeName();
+    Integer getQuantity();
+    Float getPrice();
+    Integer getDiscount();
+}
