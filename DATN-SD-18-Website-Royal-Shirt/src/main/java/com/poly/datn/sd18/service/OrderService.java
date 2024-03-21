e6012a77c860bf63987542a2a00f1be63b724972
tamdvph29277 <tamtihon27@gmail.com>
2024-03-21 15:41:18 +0700

    update

--- DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/service/OrderService.java
+++ DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/service/OrderService.java
@@ -1,10 +1,24 @@
 package com.poly.datn.sd18.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.poly.datn.sd18.entity.Order;
 import com.poly.datn.sd18.repository.OrderRepository;
 
 public interface OrderService {
+    List<Order> getAll();
+
+    Order getById(int id);
+
+    Order update(Order order);
+
+    List<Order> getByType(boolean type);
+
+    List<Order> getByStatus(int status);
+
+    List<Order> getByStatusAndType(int status, boolean type);
 
 }
--- DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/service/impl/OrderServiceImpl.java
+++ DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/service/impl/OrderServiceImpl.java
@@ -1,6 +1,9 @@
 package com.poly.datn.sd18.service.impl;
 
+import com.poly.datn.sd18.entity.Order;
+import com.poly.datn.sd18.entity.Product;
 import com.poly.datn.sd18.repository.OrderRepository;
+import com.poly.datn.sd18.repository.ProductRepository;
 import com.poly.datn.sd18.service.OrderService;
 
 import jakarta.transaction.Transactional;
@@ -8,12 +11,42 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+    @Autowired
     private final OrderRepository orderRepository;
+
+    @Override
+    public List<Order> getAll() {
+        return orderRepository.getAll();
+    }
+
+    public List<Order> getByType(boolean type) {
+        return orderRepository.getByType(type);
+    }
+
+    public Order update(Order order) {
+        return orderRepository.saveAndFlush(order);
+    }
+
+    public List<Order> getByStatus(int status) {
+        return orderRepository.getByStatus(status);
+    }
+
+    public Order getById(int id) {
+        return orderRepository.getById(id);
+    }
+
+    public List<Order> getByStatusAndType(int status, boolean type) {
+        // Implement the method
+        return orderRepository.getByStatusAndType(status, type);
+    }
 
     public Float totalOrders() {
         return orderRepository.totalOrders();
@@ -56,4 +89,5 @@ public class OrderServiceImpl implements OrderService {
     public List<Object> thongKeSoSanPham(int Nam) {
         return orderRepository.thongKeSoSanPham(Nam);
     }
+
 }
