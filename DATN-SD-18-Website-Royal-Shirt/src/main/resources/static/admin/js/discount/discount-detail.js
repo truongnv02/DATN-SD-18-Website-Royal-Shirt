ffbf0cf3ff9dce50f065e43a6e8750b3f110709c
NCTammm <nguyencongtam2k3@gmail.com>
2024-03-25 16:12:20 +0700

    update

--- DATN-SD-18-Website-Royal-Shirt/src/main/resources/static/admin/js/discount/discount-detail.js
+++ DATN-SD-18-Website-Royal-Shirt/src/main/resources/static/admin/js/discount/discount-detail.js
@@ -1,7 +1,7 @@
 var initialDiscountName = $("#discountName").val().trim();
 //Show form
 $(document).ready(function () {
-    $("#sampleTableNoneDiscount").dataTable();
+    $('#sampleTableNoneDiscount').dataTable();
 
     $('#showFormNoneDiscount').click(function () {
         $('#NoneDiscountModal').modal('show');
@@ -10,6 +10,158 @@ $(document).ready(function () {
         $('#NoneDiscountModal').modal('hide');
     });
 });
+
+//Danh sách id cartDetail được chọn để checkout
+var listProductIdAddDiscount = [];
+
+// Xử lý sự kiện khi checkbox thay đổi trạng thái
+$('.cart-checkbox-add').change(function () {
+    var productId = $(this).val(); // Lấy ID của cartDetail từ giá trị của checkbox
+
+    // Kiểm tra xem checkbox có được chọn hay không
+    if ($(this).is(':checked')) {
+        // Nếu được chọn, thêm ID vào listProductIdChoice (nếu chưa có)
+        if (!listProductIdAddDiscount.includes(productId)) {
+            listProductIdAddDiscount.push(productId);
+        }
+    } else {
+        // Nếu không được chọn, loại bỏ ID khỏi listProductIdChoice (nếu có)
+        var index = listProductIdAddDiscount.indexOf(productId);
+        if (index !== -1) {
+            listProductIdAddDiscount.splice(index, 1);
+        }
+    }
+    console.log('Danh sách thêm giảm giá:', listProductIdAddDiscount);
+});
+
+function setDiscountAll() {
+    if (listProductIdAddDiscount.length === 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Lỗi!',
+            text: 'Vui lòng chọn sản phẩm!'
+        });
+        return;
+    }
+    for (var i = 0; i < listProductIdAddDiscount.length; i++) {
+        var discountId = $("#discountForm").attr("discount-id");
+        var productId = listProductIdAddDiscount[i];
+
+        var dataToSend = {
+            discountId: discountId,
+            productId: productId
+        }
+
+        // Gửi yêu cầu AJAX
+        $.ajax({
+            type: "PUT",
+            url: "/admin/rest/discount/setDiscount",
+            contentType: "application/json",
+            data: JSON.stringify(dataToSend),
+            success: function (response) {
+                console.log("Áp dụng giảm giá thành công!");
+            },
+            error: function (error) {
+                console.error("Lỗi khi áp dụng giảm giá:", error);
+
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Lỗi!',
+                    text: 'Có lỗi xảy ra khi áp dụng giảm giá!'
+                });
+                return;
+            }
+        });
+    }
+    Swal.fire({
+        icon: 'success',
+        title: 'Thành công!',
+        text: 'Áp dụng giảm giá thành công!',
+        didClose: function () {
+            location.reload();
+        }
+    });
+}
+
+//Danh sách id cartDetail được chọn để checkout
+var listProductIdRemoveDiscount = [];
+
+// Xử lý sự kiện khi checkbox thay đổi trạng thái
+$('.cart-checkbox-remove').change(function () {
+    var productId = $(this).val(); // Lấy ID của cartDetail từ giá trị của checkbox
+
+    // Kiểm tra xem checkbox có được chọn hay không
+    if ($(this).is(':checked')) {
+        // Nếu được chọn, thêm ID vào listProductIdChoice (nếu chưa có)
+        if (!listProductIdRemoveDiscount.includes(productId)) {
+            listProductIdRemoveDiscount.push(productId);
+        }
+    } else {
+        // Nếu không được chọn, loại bỏ ID khỏi listProductIdChoice (nếu có)
+        var index = listProductIdRemoveDiscount.indexOf(productId);
+        if (index !== -1) {
+            listProductIdRemoveDiscount.splice(index, 1);
+        }
+    }
+    console.log('Danh sách gỡ giảm giá:', listProductIdRemoveDiscount);
+});
+
+function removeDiscountAll() {
+    if (listProductIdRemoveDiscount.length === 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Lỗi!',
+            text: 'Vui lòng chọn sản phẩm!'
+        });
+        return;
+    }
+
+    Swal.fire({
+        title: 'Bạn có chắc chắn muốn gỡ giảm giá?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Đồng ý',
+        cancelButtonText: 'Hủy'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            for (var i = 0; i < listProductIdRemoveDiscount.length; i++) {
+                // Nếu người dùng nhấn Đồng ý
+                var productId = listProductIdRemoveDiscount[i];
+
+                // Gửi yêu cầu AJAX
+                $.ajax({
+                    type: "PUT",
+                    url: "/admin/rest/discount/removeDiscountFromProduct/" + productId,
+                    contentType: "application/json",
+                    success: function (response) {
+                        console.log("Gỡ giảm giá thành công!");
+                    },
+                    error: function (error) {
+                        console.error("Lỗi khi gỡ giảm giá:", error);
+
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Lỗi!',
+                            text: 'Có lỗi xảy ra khi gỡ giảm giá!'
+                        });
+                        return;
+                    }
+                });
+            }
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Thành công!',
+                text: 'Gỡ giảm giá thành công!',
+                didClose: function () {
+                    location.reload();
+                }
+            });
+        }
+    });
+}
 
 function saveDiscount() {
     // Lấy dữ liệu từ biểu mẫu
